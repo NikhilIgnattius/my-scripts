7a5e82a54c8040fb156348a662b906404e68f685
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2025-01-22 21:36:30 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function contrastChanger() {
+  console.log("Contrast increased.");
+}
+
+function highlightLinks() {
+  console.log("Links highlighted.");
+}
+
+function print(message) {
+  console.log(message);
+}
