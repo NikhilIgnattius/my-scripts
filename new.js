66a0b9e66b14cf6802d123a08c4bad22b4aa29c9
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2025-01-22 16:22:03 +0530

    Update new.js

--- new.js
+++ new.js
@@ -80,7 +80,7 @@ function accessibilityButton() {
 
     // Create iframe inside the widget
     const iframe = document.createElement("iframe");
-    iframe.src = "https://www.example.com";  // URL you want to load inside iframe
+    iframe.src = "https://nikhilignattius.github.io/my-scripts/widget.js";  // URL you want to load inside iframe
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.style.border = "none";
